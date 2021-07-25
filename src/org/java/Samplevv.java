56c002049706f4b87c4fcadacd75a756dba4f25a
Tino-test <moraistrino@gmail.com>
2021-07-25 09:44:33 +0530

    equals code

---
+++ src/org/java/Samplevv.java
@@ -0,0 +1,17 @@
+package org.java;
+
+public class Samplevv {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=55;
+if(b==a) {
+	System.out.println("equal");
+}
+else {
+	System.out.println("not equal");
+}
+	}
+
+}
